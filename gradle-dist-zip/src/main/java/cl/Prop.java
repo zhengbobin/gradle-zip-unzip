abd02fd1d525b27f7e9de7085264bb56551a92ab
danielebosetti <danielebosetti2@gmail.com>
2019-07-06 20:22:28 +0100

    test

---
+++ gradle-dist-zip/src/main/java/cl/Prop.java
@@ -0,0 +1,11 @@
+package cl;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class Prop {
+}
